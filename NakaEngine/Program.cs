23c10f958334a9d72d66e20ff1095d6947a4b257
Stevie <xxlennygamerxx@gmail.com>
2021-07-16 22:26:07 -0700

    por favor

--- NakaEngine/Program.cs
+++ NakaEngine/Program.cs
@@ -3,14 +3,14 @@
 namespace NakaEngine
 {
     public static class Program
-	{
-		[STAThread] 
-		public static void Main()
-		{
-			Bootstrap.Initialize_FNA();
+    {
+        [STAThread] 
+        public static void Main()
+        {
+            Bootstrap.Initialize_FNA();
 
             NakaEngine engine = new();
             engine.Run();
         }
-	}
+    }
 }
